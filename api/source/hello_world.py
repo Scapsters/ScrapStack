5e9af2c69770174cb240c3a6b7fdce9242d64e7b
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2025-05-19 11:06:23 -0700

    added sample code, not working for now

--- api/source/hello_world.py
+++ api/source/hello_world.py
@@ -1,6 +1,21 @@
 import json
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# uri_template = "mongodb+srv://{}:{}@scrapstackcluster0.kbhra5p.mongodb.net/?retryWrites=true&w=majority&appName=ScrapstackCluster0&tlsAllowInvalidCertificates=true"
 
 def hello_world(event, context):
+    # Create a new client and connect to the server
+    # client = MongoClient(uri_template.format(event['db_username'], event['db_password']), server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+        
+    # except Exception as e:
+    #     print(e)
+
     response = {
         "statusCode": 200,
         "headers": {
@@ -13,3 +28,6 @@ def hello_world(event, context):
     }
 
     return response
+
+# these local tests don't work either on my machine
+# print(hello_world({'db_username': 'tester', 'db_password': 'yp1DVeZD1fY4zslf'}, {}))
