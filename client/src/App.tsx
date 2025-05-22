75425c9b49af4eb040536ebc9808668f7744c4be
Scapsters <scotty.happy@gmail.com>
2025-05-22 19:36:12 -0400

    scaffold react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
-function App() {
+import { useAsync } from './hooks/asyncUtils'
 
-  return (
-    <>
-      <h1 className="text-red-500">meow</h1>
-    </>
-  )
+export const API_ROOT = 'https://v8tnlmgv35.execute-api.us-east-1.amazonaws.com/scapsters-scrapstack-api-gateway'
+
+function App() {
+    const [helloData, isLoading] = useAsync<string>('/hello_world')
+	if(isLoading) return <p>Loading</p>
+	return (<>
+		<h1 className="text-red-500">meow</h1>
+		<p> {helloData} </p>
+    </>)
 }
 
 export default App
