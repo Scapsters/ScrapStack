20fa9e372eb7e34fdc69ab8476e4bbccb46d9757
Scapsters <scotty.happy@gmail.com>
2025-05-24 11:16:57 -0400

    move lambda permission granting into specific file

--- terraform/modules/lambda/hello_world.tf
+++ terraform/modules/lambda/hello_world.tf
@@ -26,3 +26,12 @@ data "archive_file" "hello_world_lambda_zip" {
 output "hello_world_invoke_arn" {
   value = aws_lambda_function.hello_world.invoke_arn
 }
+
+# give gateway invokers the lambda invoke policy
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.api_gateway_execution_arn}/*/*"
+}
--- terraform/modules/lambda/lambda.tf
+++ terraform/modules/lambda/lambda.tf
@@ -70,15 +70,6 @@ resource "aws_iam_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# give gateway invokers the lambda invoke policy
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${var.api_gateway_execution_arn}/*/*"
-}
-
 module "api" {
   source = "api"
   python_runtime = var.python_runtime
