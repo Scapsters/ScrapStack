9e7fd5cb633111b7549099b8e42bbbdaf3dd47ee
Scapsters <scotty.happy@gmail.com>
2025-05-24 11:00:51 -0400

    specify output name

--- terraform/modules/gateway/output.tf
+++ terraform/modules/gateway/output.tf
@@ -2,6 +2,6 @@ output "api_gateway_execution_arn" {
   value = aws_api_gateway_rest_api.scrapstack.execution_arn
 }
 
-output "api_gateway_endpoint_url" {
+output "api_gateway_hello_world_endpoint_url" {
   value = "https://${aws_api_gateway_rest_api.scrapstack.id}.execute-api.us-east-1.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}/hello-world"
 }
