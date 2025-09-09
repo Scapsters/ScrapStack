d6af1c932ccd53e9b2b1f35d6a277b4d0800f89c
Scapsters <scotty.happy@gmail.com>
2025-09-08 23:06:02 -0400

    temp

--- client/src/StackSearch.tsx
+++ client/src/StackSearch.tsx
@@ -1,24 +1,23 @@
 import { useMemo, useState } from "react"
 import { TopBar } from "./LandingPage"
-import { Input } from '@headlessui/react'
+import { Field, Input } from '@headlessui/react'
 import { trpc } from "./trpc"
 import { useQuery } from "@tanstack/react-query"
 import FuzzySearch from 'fuzzy-search';
-import { StackDB } from '../../api/source/schemas';
 
 export default function StackSearch() {
     const [search, setSearch] = useState("")
     
-    const { data, error } = useQuery(trpc.getStacks.queryOptions())
+    const { data: stacks, error } = useQuery(trpc.getStacks.queryOptions())
     const searchedStacks = useMemo(
-        () => data && new FuzzySearch(data, [StackDB]), 
-        [stacks]
+        () => (stacks && new FuzzySearch(stacks, ['twitterHandle', "_id"]).search(search)) ?? [], 
+        [search, stacks]
     )
         
     return (<>
         <TopBar />
-        <div className="flex flex-col w-full h-100 bg-red align-center">
-            <Input name="Search Stacks"/>
+        <div className="flex flex-col w-full h-100 align-center">
+            <Input placeholder="Search Stacks" className="border-2 w-200 border-dark/40"/>
         </div>
     </>)
 }
