3703659e13fcc346863977d3abccd8bcd8c77a1c
Scott Happy <sdhhelp@rit.edu>
2025-09-29 15:11:38 -0400

    remove redundant comment

--- api/source/api/db.ts
+++ api/source/api/db.ts
@@ -10,13 +10,13 @@ export async function queryRandomTweets(
     const tweetIds = await Tweet.find(filter).project<{ _id: ObjectId }>({ _id: 1 }).toArray()
     const sampledIds: ObjectId[] = []
     const length = tweetIds.length
-    for (let i = 0; i < Math.min(5, length); i++) { // Sample either 20 or the amount of IDs, whichever is smaller
+    for (let i = 0; i < Math.min(5, length); i++) {
         sampledIds.push(tweetIds.splice(Math.floor(Math.random() * tweetIds.length), 1)[0]._id) // Remove IDs as they are sampled
     }
     const sampledTweets = await Tweet.find({ _id: { $in: sampledIds } }).toArray()
     if (user) {
         await User.updateOne(user, {
-            $push: { sentPosts: { $each: sampledTweets.map(tweet => tweet.tweet_id) } }
+            $push: { sentPosts: { $each: sampledTweets.map(tweet => tweet._id) } }
         })
     }
     return sampledTweets
