a6f88ee6843c34a4ec0a76523a5951f6a56c5f32
Scott Happy <sdhhelp@rit.edu>
2025-09-29 20:11:52 -0400

    improve random tweet fetching

--- api/source/api/db.ts
+++ api/source/api/db.ts
@@ -1,21 +1,21 @@
 import { Collection, ObjectId, type StrictFilter } from "mongodb"
-import type { TweetSchema, UserSchema } from "./schemas.js"
+import type { Tweet, TweetSchema, User, UserSchema } from "./schemas.js"
 
 export async function queryRandomTweets(
     Tweet: Collection<TweetSchema>,
     User: Collection<UserSchema>,
     filter: StrictFilter<TweetSchema>, 
-    user: UserSchema | null
+    user: User | null
 ) {
-    const tweetIds = await Tweet.find(filter).project<{ _id: ObjectId }>({ _id: 1 }).toArray()
-    const sampledIds: ObjectId[] = []
-    const length = tweetIds.length
-    for (let i = 0; i < Math.min(5, length); i++) {
-        sampledIds.push(tweetIds.splice(Math.floor(Math.random() * tweetIds.length), 1)[0]._id) // Remove IDs as they are sampled
-    }
-    const sampledTweets = await Tweet.find({ _id: { $in: sampledIds } }).toArray()
+    const count = await Tweet.countDocuments(filter);
+    if (count === 0) return [];
+    const sampledTweets = await Tweet.aggregate([
+        { $match: filter },
+        { $sample: { size: Math.min(5, count) } }
+    ]).toArray() as Tweet[]
+
     if (user) {
-        await User.updateOne(user, {
+        await User.updateOne({ _id : user._id }, {
             $push: { sentPosts: { $each: sampledTweets.map(tweet => tweet._id) } }
         })
     }
