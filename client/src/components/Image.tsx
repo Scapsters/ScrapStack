f4ed6fb3da081549f621e3ebd6f7522c33119886
Scapsters <scotty.happy@gmail.com>
2026-05-11 10:11:13 -0400

    format and menial refactor

--- client/src/components/Image.tsx
+++ client/src/components/Image.tsx
@@ -1,40 +1,35 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from 'react'
 
-export default function Image({ ...props }: React.ImgHTMLAttributes<HTMLImageElement> & { onLoad?: () => void }) {
-    const [fullscreen, setFullscreen] = useState(false)
+export default function FullscreenableImage({ ...props }: React.ImgHTMLAttributes<HTMLImageElement>) {
+	const [fullscreen, setFullscreen] = useState(false)
 
-    const imageRef = useRef<HTMLImageElement>(null)
-    const ref = useRef<HTMLDivElement>(null)
+	const imageRef = useRef<HTMLImageElement>(null)
+	const ref = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const current = ref.current
-        if (!current) return
+	useEffect(() => {
+		const current = ref.current
+		if (!current) return
 
-        const blockScroll = (e: Event) => { 
-            if (fullscreen) e.preventDefault()
-        }
+		const blockScroll = (e: Event) => {
+			if (fullscreen) e.preventDefault()
+		}
 
-        current.addEventListener("wheel", blockScroll)
-        return () => current.removeEventListener("wheel", blockScroll)
-    }, [fullscreen])
+		current.addEventListener('wheel', blockScroll)
+		return () => current.removeEventListener('wheel', blockScroll)
+	}, [fullscreen])
 
-    return (
-        <div
-            ref={ref}
-            onClick={() => setFullscreen(!fullscreen)}
-        >
-            <div className={fullscreen
-                ? `z-40 fixed top-0 left-0 w-screen h-[300dvh] bg-black/20`
-                : `hidden`
-            }>
-            </div>
-            <img
-                ref={imageRef}
-                {...props}
-                className={`${fullscreen
-                    ? "z-50 fixed max-h-screen max-w-screen top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
-                    : "min-w-0 max-w-[90dvw] lg:max-w-[40dvw] max-h-full rounded-lg border border-black/10"
-                    }`}
-            />
-        </div>)
+	return (
+		<div ref={ref} onClick={() => setFullscreen(!fullscreen)}>
+			{fullscreen && <div className="z-40 fixed top-0 left-0 w-screen h-[300dvh] bg-black/20" />}
+			<img
+				ref={imageRef}
+				{...props}
+				className={`${
+					fullscreen
+						? 'z-50 fixed max-h-screen max-w-screen top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'
+						: 'min-w-0 max-w-[90dvw] lg:max-w-[40dvw] max-h-full rounded-lg border border-black/10'
+				}`}
+			/>
+		</div>
+	)
 }
